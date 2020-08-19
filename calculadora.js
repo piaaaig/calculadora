9977a324fdec2d0994a8c170805506a95261439c
Pig Nachos <piaignaciara@gmail.com>
2020-08-19 19:59:48 -0400

    botones de numeros

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,27 @@
-function init(){
+const init = () => {
+    const resultado = document.getElementById("resultado");
+    let operacion = "";
+    let tempResultado = "";
+    
+    [
+        "cero",
+        "uno",
+        "dos",
+        "tres",
+        "cuatro",
+        "cinco",
+        "seis",
+        "siete",
+        "ocho",
+        "nueve",
+    ]
+    .forEach((número, i) => document.getElementById(número).addEventListener("click", () => resultado.value += `${i}`));
+    
+    document.getElementById("suma").addEventListener("click", () => {
+        operacion = "suma";
+        tempResultado = resultado.value;
+        resultado.value = "";
+    });
 
 }
+
